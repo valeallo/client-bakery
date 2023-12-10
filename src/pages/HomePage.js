cc435fb1febc353e97e7ffe92ef329225bd48019
valeallo <valentina.allodi@gmail.com>
2023-12-10 15:31:55 +0100

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,7 @@ import AllPastries from '../components/AllPastries'
 const HomePage = () => {
   // PUNTO 6 Realizzare una pagina vetrina dove tutti possono vedere la lista di dolci disponibili e il
   // prezzo relativo.
-  
+
   return (
     <>
      <Hero />
