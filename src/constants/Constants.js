6c5a90e1bde5b850182d3d1d35d2bd54fd85f62c
valeallo <valentina.allodi@gmail.com>
2024-10-09 16:02:00 +0200

    ok
    ok

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -29,5 +29,6 @@ export const FETCH_PASTRIES_START = 'FETCH_PASTRIES_START';
 export const FETCH_PASTRIES_SUCCESS = 'FETCH_PASTRIES_SUCCESS';
 export const FETCH_PASTRIES_FAILURE = 'FETCH_PASTRIES_FAILURE';
 
-export const FETCH_PASTRIES_API = process.env.REACT_APP_API_BASE_URL + "/pastries";
-export const LOGIN_API =  process.env.REACT_APP_API_BASE_URL + "/login";
+const DOT_ENV = process.env.REACT_APP_API_BASE_URL;
+export const FETCH_PASTRIES_API = DOT_ENV + "/pastries";
+export const LOGIN_API =  DOT_ENV + "/login";
